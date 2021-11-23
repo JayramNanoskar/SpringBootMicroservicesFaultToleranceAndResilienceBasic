21b50dd6aeae97865dc29a3e9e90cd0bf99dde07
JayramNanoskar <jayram.n978@gmail.com>
2021-11-23 22:23:34 +0530

    Configuring Bulkhead pattern

--- movie-catalog-service/src/main/java/com/jayram/moviecatalogservice/services/MovieInfo.java
+++ movie-catalog-service/src/main/java/com/jayram/moviecatalogservice/services/MovieInfo.java
@@ -8,6 +8,7 @@ import com.jayram.moviecatalogservice.models.CatalogItem;
 import com.jayram.moviecatalogservice.models.Movie;
 import com.jayram.moviecatalogservice.models.Rating;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class MovieInfo {
@@ -15,7 +16,12 @@ public class MovieInfo {
 	@Autowired
 	RestTemplate restTemplate;
 	
-	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem",
+			threadPoolKey = "movieInfoPool",
+			threadPoolProperties = {
+					@HystrixProperty(name = "coreSize", value = "20"),
+					@HystrixProperty(name = "maxQueueSize", value = "10")
+			})
 	public CatalogItem getCatalogItem(Rating rating) {
 		Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+ rating.getMovieId(), Movie.class); //Unmarshalling of response into Movie class instance
 		return new CatalogItem(movie.getName(), movie.getDesciption(), rating.getRating());
